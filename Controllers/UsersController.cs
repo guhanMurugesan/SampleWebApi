3995d23e30d6ed7242629ae53abd91a07d84e6b3
guhanMurugesan <mguhan.rmd@gmail.com>
2016-06-28 19:09:34 +0530

    intial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using SampleWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleWebApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        UserModel[] users = new UserModel[]
+        {
+            new UserModel(){ UserId = 100, Username = "guhan", Location = "chennai", Age = 23},
+            new UserModel(){UserId = 101, Username = "Bala",Location="chennai",Age=22}
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<UserModel> GetAllUsers()
+        {
+            return users;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IHttpActionResult GetUser(long id)
+        {
+            var user = users.FirstOrDefault((u) => u.UserId == id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ Scripts/base.js
@@ -0,0 +1,13 @@
+﻿var apiUrl = 'api/users';
+
+$(document).ready(function () {
+    $.getJSON(apiUrl).done(function (data) {
+        $.each(data, function (key, item) {
+            $('<li>', { text: formatItem(item) }).appendTo($('#users'));
+        })
+    });
+})
+
+function formatItem(item) {
+    return item.Username + ':' + item.Age;
+}
